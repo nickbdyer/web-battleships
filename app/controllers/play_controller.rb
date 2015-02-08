fd997cb5be53f817fb250e5bbbdad16a6cc84eb2
Nick Dyer <nbdyer@gmail.com>
2015-02-08 19:26:43 +0000

    Refactor play controller with before method

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -1,14 +1,15 @@
 class Battleships < Sinatra::Base
 
-  get '/play' do
+  before '/play' do
     @player = GAME.players.select { |player| player.object_id == session[:me] }.first
     @opponent = GAME.players.select { |player| player.object_id != session[:me] }.first
+  end
+
+  get '/play' do
     erb :play, :layout => :layout_refresh
   end
 
   post '/play' do
-    @player = GAME.players.select { |player| player.object_id == session[:me] }.first
-    @opponent = GAME.players.select { |player| player.object_id != session[:me] }.first
     shot_coords = (params[:x_coords] + params[:y_coords]).to_sym
     begin
       GAME.shoots(shot_coords) 
